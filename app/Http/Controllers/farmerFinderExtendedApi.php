76e9aab83e7d6b0bc6f4c70976384f394ea51fb0
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2024-04-11 11:44:16 +0800

    Updated KP Encoding API

--- app/Http/Controllers/farmerFinderExtendedApi.php
+++ app/Http/Controllers/farmerFinderExtendedApi.php
@@ -515,8 +515,8 @@ class farmerFinderExtendedApi extends Controller{
             }
             $parsed_prv = explode('-',$row->rsbsa_control_no);
 
-            $prv = $prv[0];
-            $prv = $prv.(count($prv[1]) == 2? $prv[1] : substr($prv[1], 1));
+            $prv = $parsed_prv[0];
+            $prv = $prv.(count($parsed_prv[1]) == 2? $parsed_prv[1] : substr($parsed_prv[1], 1));
             // $prv = substr(str_replace('-','',$row->rsbsa_control_no),0,4);
             $season = strtolower($row->season);
             $validate = DB::table('kp_distribution.kp_distribution_app')
