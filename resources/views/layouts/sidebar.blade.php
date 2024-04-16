f026f5532c8e8f051d13b25eea961d2f01ac3304
Bryan Michael S. Delos Santos <bmsdelossantos@philrice.local>
2024-04-16 10:32:42 +0800

    sidebar access

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1422,7 +1422,7 @@ if($_SERVER['REQUEST_URI']!="/rcef_ds2024/pre_reg/view_farmer"){
 									<li class="sub_menu"><a href="{{route('paymaya.municipalities.list')}}">Municipalities</a></li>
 									@endif
 								 @endif
-								  @if(Auth::user()->username == "jpalileo" || Auth::user()->username == "r.benedicto_2" || Auth::user()->username == "reggie_dioses" ||  Auth::user()->username == "dc.gaspar" ||  Auth::user()->username == "jg.villanueva"||  Auth::user()->username == "e.lopez"||  Auth::user()->username == "jt.rivera" || Auth::user()->username == "renaida_pascual" || Auth::user()->username == "processor_jbl" || Auth::user()->username == "ar.aromin"|| Auth::user()->username == "ar.aromin1"|| Auth::user()->username == "danrio"|| Auth::user()->username == "bryan0629")
+								  @if(Auth::user()->username == "jpalileo" || Auth::user()->username == "r.benedicto_2" || Auth::user()->username == "reggie_dioses" ||  Auth::user()->username == "dc.gaspar" ||  Auth::user()->username == "jg.villanueva"||  Auth::user()->username == "e.lopez"||  Auth::user()->username == "jt.rivera" || Auth::user()->username == "renaida_pascual" || Auth::user()->username == "processor_jbl" || Auth::user()->username == "ar.aromin"|| Auth::user()->username == "ar.aromin1"|| Auth::user()->username == "danrio"|| Auth::user()->username == "bryan0629" || Auth::user()->username == "ddc.espiritu")
 								 
 								  
 							
